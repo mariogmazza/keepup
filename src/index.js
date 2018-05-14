6477a7033a39b98a541d8f4109248ad7142f63f6
mariogmazza <mariomazza01@gmail.com>
2018-05-13 22:58:38 -0400

    working on the navBar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css'
 import './index.css';
-import App from './App';
+import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEL = document.getElementById('root');
+
+let render = () =>{
+    ReactDOM.render(<App />, rootEL)
+}
+
+if(module.hot){
+    module.hot.accept('./app/layout/App', ()=>{
+        setTimeout(render);
+    })
+}
+
+render();
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
